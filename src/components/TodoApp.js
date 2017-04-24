42f0e8843ce4c269fc0e532c706a9e5a72702295
Brian Smith <briaguya@gmail.com>
2017-04-24 18:19:34 -0500

    Added some react router stuff, working towards auth

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react'
 import TodoListFooter from './TodoListFooter'
 import AddTodo from './AddTodo'
 import TodoList from './TodoList'
+import { withRouter } from 'react-router'
 import gql from 'graphql-tag'
 
 import { connect } from 'react-redux'
@@ -13,37 +14,123 @@ class TodoApp extends React.Component {
     renameTodo: PropTypes.func.isRequired,
     deleteTodo: PropTypes.func.isRequired,
     toggleTodo: PropTypes.func.isRequired,
+    router: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
     filter: PropTypes.string.isRequired,
     setFilter: PropTypes.func.isRequired,
   }
 
+  _logout = () => {
+    // remove token from local storage and reload page to reset apollo client
+    window.localStorage.removeItem('graphcoolToken')
+    location.reload()
+  }
+
+  _showLogin = () => {
+    this.props.router.push('/login')
+  }
+
+  _showSignup = () => {
+    this.props.router.push('/signup')
+  }
+
+  _isLoggedIn = () => {
+    return this.props.data.user
+  }
+
   render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+
+    if (this._isLoggedIn()) {
+      return this.renderLoggedIn()
+    } else {
+      return this.renderLoggedOut()
+    }
+  }
+
+  renderLoggedIn() {
     return (
       <div>
-        <section className='todoapp'>
-          <header className='header'>
-            <AddTodo
-              addTodo={this.props.addTodo}
+        <span>
+          Logged in as {this.props.data.user.name}
+        </span>
+        <div className='pv3'>
+          <span
+            className='dib bg-red white pa3 pointer dim'
+            onClick={this._logout}
+          >
+            Logout
+          </span>
+        </div>
+
+        <div>
+          <section className='todoapp'>
+            <header className='header'>
+              <AddTodo
+                addTodo={this.props.addTodo}
+                refetch={this.props.data.refetch}
+              />
+            </header>
+            <TodoList
+              todos={this.props.data.allTodoes || []}
+              filter={this.props.filter}
+              renameTodo={this.props.renameTodo}
+              deleteTodo={this.props.deleteTodo}
+              toggleTodo={this.props.toggleTodo}
               refetch={this.props.data.refetch}
+              loading={this.props.data.loading}
             />
-          </header>
-          <TodoList
-            todos={this.props.data.allTodoes || []}
-            filter={this.props.filter}
-            renameTodo={this.props.renameTodo}
-            deleteTodo={this.props.deleteTodo}
-            toggleTodo={this.props.toggleTodo}
-            refetch={this.props.data.refetch}
-            loading={this.props.data.loading}
-          />
-          <TodoListFooter setFilter={this.props.setFilter} />
-        </section>
-        <footer className='info'>
-          <p>
-            Double-click to edit a todo
-          </p>
-        </footer>
+            <TodoListFooter setFilter={this.props.setFilter} />
+          </section>
+          <footer className='info'>
+            <p>
+              Double-click to edit a todo
+            </p>
+          </footer>
+        </div>
+
+      </div>
+    )
+  }
+
+  renderLoggedOut() {
+    return (
+      <div>
+        <div className='pv3'>
+          <div>
+            <span
+              onClick={this._showLogin}
+              className='dib pa3 white bg-blue dim pointer'
+            >
+              Log in with Email
+            </span>
+          </div>
+          <span>Log in to create new todos</span>
+          <div>
+            <span
+              onClick={this._showSignup}
+              className='dib pa3 white bg-blue dim pointer'
+            >
+              Sign up with Email
+            </span>
+          </div>
+        </div>
+        <div>
+          <section className='todoapp'>
+            <TodoList
+              todos={this.props.data.allTodoes || []}
+              filter={this.props.filter}
+              renameTodo={this.props.renameTodo}
+              deleteTodo={this.props.deleteTodo}
+              toggleTodo={this.props.toggleTodo}
+              refetch={this.props.data.refetch}
+              loading={this.props.data.loading}
+            />
+            <TodoListFooter setFilter={this.props.setFilter} />
+          </section>
+        </div>
       </div>
     )
   }
@@ -105,4 +192,13 @@ const TodoAppLinked = connect(
   }),
 )(withQueryAndMutations)
 
-export default TodoAppLinked
+const userQuery = gql`
+  query {
+    user {
+      id
+      name
+    }
+  }
+`
+
+export default graphql(userQuery, { options: {forceFetch: true }})(withRouter(TodoAppLinked))
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import TodoApp from './components/TodoApp'
 
+import CreateUser from './components/CreateUser'
+import LoginUser from './components/LoginUser'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
@@ -11,6 +15,20 @@ import './style.css'
 // Paste your endpoint for the Simple API here.
 // Info: https://github.com/graphcool-examples/react-apollo-todo-example#2-create-graphql-api-with-graphcool
 const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj1sgcb2r1lp70109cqcaq449' })
+
+networkInterface.use([{
+  applyMiddleware (req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+
+    // get the authentication token from local storage if it exists
+    if (localStorage.getItem('graphcoolToken')) {
+      req.options.headers.authorization = `Bearer ${localStorage.getItem('graphcoolToken')}`
+    }
+    next()
+  },
+}])
 
 const client = new ApolloClient({
   networkInterface,
@@ -39,7 +57,13 @@ const store = createStore(
 
 render(
   <ApolloProvider store={store} client={client}>
-    <TodoApp />
+    <Router>
+      <div>
+        <Route path='/' component={TodoApp} />
+        <Route path='login' component={LoginUser} />
+        <Route path='signup' component={CreateUser} />
+      </div>
+    </Router>
   </ApolloProvider>,
   document.getElementById('root')
 )
